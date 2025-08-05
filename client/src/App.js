6f7328b1b800b86a1957c9ed64ff2d6197180479
Mohdsohail7 <sohailmohd437@gmail.com>
2025-08-05 20:38:06 +0530

    feat: feed page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/feed" element = {<Feed/>} />
         <Route path = "/register" element = {<Register/>} />
         <Route path = "/login" element = {<Login/>} />
       </Routes>
